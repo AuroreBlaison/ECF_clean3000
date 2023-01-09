dde532fd10e030eded1c48e3242043520394b407
SamDWWM <samuel.cloquie@protonmail.com>
2023-01-09 11:55:29 +0100

    mise en place page d'accueil + style

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../img/logo-clean3000-transparent.png'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img className='logo' src={logo} alt="logo clean3000"></img>
+        </div>
+    )
+}
+
+export default Header
