14e2a5fb1b3cdff44342ae587a8c0b8a7dbf5131
SamDWWM <samuel.cloquie@protonmail.com>
2023-01-09 17:22:15 +0100

    mise en place du recap

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -3,7 +3,7 @@ import Footer from './Footer';
 import Header from './Header';
 
 const Formulaire = () => {
-   
+
 
     const [presta, setPresta] = useState({
         clientName: "",
@@ -12,37 +12,43 @@ const Formulaire = () => {
         tech: "",
     })
 
-    const addPresta = () => {
-     const newPresta = {
-        clientName: "",
-        date: "",
-        observations: "pas d'observations",
-        tech: "",
-     }
+    // const addPresta = () => {
+    //  const newPresta = {
+    //     clientName: "",
+    //     date: "",
+    //     observations: "pas d'observations",
+    //     tech: "",
+    //  }
 
-     setPresta(
-        [...presta, newPresta]
-        )
-    };
-
-    // const NewPresta = (e) => {
-    //     const copyPresta = { ...presta };
-    //     // copyPresta = e.target.value;
-    //     copyPresta.clientName = e.target.client.value;
-    //     copyPresta.date=e.target.date.value;
-    //     copyPresta.observations= e.target.comments.value;
-    //     copyPresta.tech=e.target.tech.value;
-
-    //     setPresta(
-    //         {
-    //             ...presta, copyPresta
-    //         }
+    //  setPresta(
+    //     [...presta, newPresta]
     //     )
-    // }
+    // };
+
+    const renderPresta = () => {
+        return (
+            <div>
+                <ul>
+                    <li>
+                        {presta.clientName}
+                    </li>
+                    <li>
+                        {presta.date}
+                    </li>
+                    <li>
+                        {presta.observations}
+                    </li>
+                    <li>
+                        {presta.tech}
+                    </li>
+                </ul>
+            </div>
+        )
+    }
 
     return (
         <div className="App">
-            <Header 
+            <Header
             />
 
             <div className="form-group">
@@ -51,7 +57,7 @@ const Formulaire = () => {
                         <input
                             type="text"
                             className="client"
-                            onChange={(e) => setPresta({...presta, clientName :e.target.value})}
+                            onChange={(e) => setPresta({ ...presta, clientName: e.target.value })}
                         />
                     </label>
 
@@ -59,35 +65,38 @@ const Formulaire = () => {
                         <input
                             type="date"
                             className="date"
-                            onChange={(e) => setPresta({...presta, date :e.target.value})}
+                            onChange={(e) => setPresta({ ...presta, date: e.target.value })}
                         />
                     </label>
                     <label> Observations :
                         <textarea
                             type="text"
                             className="comments"
-                            onChange={(e) => setPresta({...presta, observations :e.target.value})}
+                            onChange={(e) => setPresta({ ...presta, observations: e.target.value })}
                         />
                     </label>
                     <label> Nom du technicien :
                         <input
                             type="text"
                             className="tech"
-                            onChange={(e) => setPresta({...presta, tech :e.target.value})}
+                            onChange={(e) => setPresta({ ...presta, tech: e.target.value })}
                         />
                     </label>
 
                     <button
-
                         className="btn"
-                        // onSubmit={(e) => NewPresta(e)}
-                        onClick={() => addPresta()}
+                    // onClick={() => renderPresta()}
                     >
                         Enregistrer
                     </button>
                 </form>
-
             </div>
+
+            <div className='recap'>
+                <h1>Recap</h1>
+                {renderPresta()}
+            </div>
+
 
             <Footer />
         </div>
